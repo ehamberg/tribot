fdc3740a06727cd2f91d77d6a787f3213e51deaf
Erlend Hamberg <ehamberg@gmail.com>
2011-03-02 14:39:15 +0100

    skeleton

---
+++ src/bibot.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Network.SimpleIRC
+import Data.Maybe
+import qualified Data.ByteString.Char8 as B
+
+mkRandSentence = "foo"
+
+storeSentence m = return ()
+
+onMessage :: EventFunc
+onMessage s m
+  | nick `B.isInfixOf` msg = sendMsg s chan mkRandSentence
+  | otherwise = storeSentence msg
+  where chan = fromJust $ mChan m
+        msg = mMsg m
+        nick = B.pack (cNick conf)
+
+events :: [IrcEvent]
+events = [(Privmsg onMessage)]
+
+conf :: IrcConfig
+conf = defaultConfig
+  { cAddr = "ipv6.chat.freenode.net" -- Address
+  , cNick = "bibot"                  -- Nickname
+  , cChannels = ["#bibot"]           -- Channels to join on connect
+  , cEvents = events                 -- Events to bind
+  }
+
+main :: IO (Either IOError MIrc)
+main = do
+  connect conf False True
