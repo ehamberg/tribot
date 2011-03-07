a3d1b5a357a2474f3e1c06fe931de71698122679
Erlend Hamberg <ehamberg@gmail.com>
2011-03-07 14:49:53 +0100

    store bigrams and start/end words of a sentece

--- src/bibot.hs
+++ src/bibot.hs
@@ -8,64 +8,74 @@ import qualified Database.HDBC.Sqlite3 as DB
 import Control.Monad (when)
 import Data.List (find)
 
+botIrcName     = "bibot"
+botIrcServer   = "ipv6.chat.freenode.net"
+botIrcChannels = ["#bitbot"]
+
+bigrams :: [a] -> [[a]]
+bigrams [x] = []
+bigrams xs  = take 2 xs : bigrams (tail xs)
+
 mkRandSentence = "foo"
 
-storeSentence m = return ()
+storeSentence :: DB.Connection -> B.ByteString -> IO ()
+storeSentence db s = do
+  DB.run db (insert "startword") [firstW]
+  DB.run db (update "startword") [firstW]
+  DB.run db (insert "endword") [lastW]
+  DB.run db (update "endword") [lastW]
+  mapM_ (\[w1,w2] -> addBigram db w1 w2) $ bigrams tokens
+  DB.commit db
+    where tokens   = filter (/="") $ B.split ' ' s
+          firstW   = (DB.toSql . B.unpack . head) tokens -- first word
+          lastW    = (DB.toSql . B.unpack . last) tokens -- last word
+          insert t = "INSERT OR IGNORE INTO " ++ t ++ " VALUES (?, 0)"
+          update t = "UPDATE " ++ t ++ " set count = count+1 where word=?";
+
+addBigram :: DB.Connection -> B.ByteString -> B.ByteString -> IO ()
+addBigram db word1 word2 = do
+  DB.run db "INSERT OR IGNORE INTO bigram VALUES (?, ?, 0)" words
+  DB.run db "UPDATE bigram set count = count+1 where w1=? and w2=?" words
+  return ()
+    where bToSql = DB.toSql . B.unpack
+          words = [bToSql word1, bToSql word2]
 
 onMessage :: EventFunc
 onMessage s m
-  | nick `B.isInfixOf` msg = sendMsg s chan mkRandSentence
-  | otherwise = storeSentence msg
+  | nick `B.isInfixOf` msg = sendMsg s chan msg
+  | otherwise = storeSentence undefined msg
   where chan = fromJust $ mChan m
         msg = mMsg m
-        nick = B.pack (cNick conf)
-
-events :: [IrcEvent]
-events = [Privmsg onMessage]
-
-conf :: IrcConfig
-conf = defaultConfig
-  { cAddr = "ipv6.chat.freenode.net" -- Address
-  , cNick = "bibot"                  -- Nickname
-  , cChannels = ["#bibot"]           -- Channels to join on connect
-  , cEvents = events                 -- Events to bind
-  }
+        nick = B.pack botIrcName
 
 mkTable :: DB.Connection -> IO ()
 mkTable db = do
   let q1 = "CREATE TABLE bigram(w1 VARCHAR(64), w2 VARCHAR(64), count INTEGER, "
          ++ "PRIMARY KEY (w1,w2))"
-  let q2 = "CREATE TABLE startwords(word VARCHAR(64), count INTEGER, "
+  let q2 = "CREATE TABLE startword(word VARCHAR(64), count INTEGER, "
          ++ "PRIMARY KEY (word))"
-  let q3 = "CREATE TABLE endwords(word VARCHAR(64), count INTEGER, "
+  let q3 = "CREATE TABLE endword(word VARCHAR(64), count INTEGER, "
          ++ "PRIMARY KEY (word))"
   putStrLn "creating database tables:"
   putStrLn "\tcreating table \"bigram\"..."
   DB.run db q1 []
-  putStrLn "\tcreating table \"startwords\"..."
+  putStrLn "\tcreating table \"startword\"..."
   DB.run db q2 []
-  putStrLn "\tcreating table \"endwords\"..."
+  putStrLn "\tcreating table \"endword\"..."
   DB.run db q3 []
   DB.commit db
 
-addBigram :: DB.Connection -> B.ByteString -> B.ByteString -> IO ()
-addBigram db word1 word2 = do
-  DB.run db (insert w1' w2') []
-  DB.run db (update w1' w2') []
-  DB.commit db
-    where w1' = B.unpack word1
-          w2' = B.unpack word2
-          insert w1 w2 = "INSERT OR IGNORE INTO bigram (w1, w2, count)"
-                       ++ " VALUES (\"" ++ w1 ++ "\", \"" ++ w2 ++ "\", 0);"
-          update w1 w2 = " UPDATE bigram set count = count+1 where w1=\""
-                       ++ w1 ++ "\" and w2=\"" ++ w2 ++ "\"";
+conf = defaultConfig
+         { cAddr     = botIrcServer        -- Address
+         , cNick     = botIrcName          -- Nickname
+         , cChannels = botIrcChannels      -- Channels to join
+         , cEvents   = [Privmsg onMessage]
+         }
 
 main :: IO ()
 main = do
   db <- DB.connectSqlite3 "bigrams.db"
   tables <- DB.getTables db
   when ((isNothing . find (=="bigram")) tables) (mkTable db)
-  addBigram db "hei" "du"
-  addBigram db "æøå" "lol"
   connect conf False True
   DB.disconnect db
